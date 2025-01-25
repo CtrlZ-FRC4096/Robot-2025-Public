c99660cb71b534339fd5ac3b47b03ade629d3c85
Miles Bimrose <milesbimrose@gmail.com>
2025-01-25 16:52:38 -0600

    added Reef class + math functions

--- robot/field_const.py
+++ robot/field_const.py
@@ -23,13 +23,15 @@ PDP = Power Distribution Panel
 
 import math
 
-from wpimath.geometry import Rotation2d, Translation2d, Pose2d
+from wpimath.geometry import Rotation2d, Rotation3d, Translation2d, Translation3d, Pose2d, Pose3d, Transform2d
 from wpimath.kinematics import SwerveDrive4Kinematics
 
 from phoenix6 import signals
 
 def inchesToMeters(inch):
     return inch/39.37
+def degreesToRadians(deg):
+    return deg * math.pi/180
 
 class FieldConstants():
     fieldLength = inchesToMeters(690.876)
@@ -45,15 +47,107 @@ class FieldConstants():
         closeCage = Translation2d() #cage closest to outside wall
         
     class CoralStation:
-        
-    class Reef:
-        
-    class StagingPositions:
-        
-    class ReefHeight:
-    
-    
-        
-        
-        
+        leftCenterFace = Pose2d(
+            inchesToMeters(33.526), 
+            inchesToMeters(291.176),
+            Rotation2d.fromDegrees(90 - 144.011)
+        )
+        rightCenterFace = Pose2d(
+            inchesToMeters(33.526), 
+            inchesToMeters(25.824),
+            Rotation2d.fromDegrees(144.011 - 90)
+        )
 
+    class Reef:
+        center = Translation2d(inchesToMeters(176.746), inchesToMeters(158.501))
+        faceToZoneLine = inchesToMeters(12) # Side of the reef to the inside of the reef zone line
+        centerFaces = [0, 0, 0, 0, 0, 0] # Starting facing the driver station in clockwise order
+        centerFaces[0] = Pose2d(
+              inchesToMeters(144.003),
+              inchesToMeters(158.500),
+              Rotation2d.fromDegrees(180)
+            )
+        centerFaces[1] = Pose2d(
+                inchesToMeters(160.373),
+                inchesToMeters(186.857),
+                Rotation2d.fromDegrees(120)
+            )
+        centerFaces[2] = Pose2d(
+                inchesToMeters(193.116),
+                inchesToMeters(186.858),
+                Rotation2d.fromDegrees(60)
+            )
+        centerFaces[3] = Pose2d(
+                inchesToMeters(209.489),
+                inchesToMeters(158.502),
+                Rotation2d.fromDegrees(0)
+            )
+        centerFaces[4] = Pose2d(
+                inchesToMeters(193.118),
+                inchesToMeters(130.145),
+                Rotation2d.fromDegrees(-60)
+            )
+        centerFaces[5] = Pose2d(
+                inchesToMeters(160.375),
+                inchesToMeters(130.144),
+                Rotation2d.fromDegrees(-120)
+            )
+        branchPositions = []
+        for face in range(6):
+            fillRight = []
+            fillLeft = []
+            for level in [(inchesToMeters(72), -90), (inchesToMeters(47.625), -35), (inchesToMeters(31.875), -35), (inchesToMeters(18), 0)]:
+                poseDirection = Pose2d(center, Rotation2d.fromDegrees(180 - (60 * face)))
+                adjustX = inchesToMeters(30.38)
+                adjustY = inchesToMeters(6.469)
+                fillRight.append(
+                    level,
+                    Pose3d(
+                        Translation3d(
+                            poseDirection.transformBy(Transform2d(adjustX, adjustY, Rotation2d())).X(),
+                            poseDirection.transformBy(Transform2d(adjustX, adjustY, Rotation2d())).Y(),
+                            level[0]
+                        ),
+                        Rotation3d(
+                            0,
+                            degreesToRadians(level[1]),
+                            poseDirection.rotation().radians()
+                        )
+                    )
+                )
+                fillLeft.append(
+                    level,
+                    Pose3d(
+                        Translation3d(
+                            poseDirection.transformBy(Transform2d(adjustX, -adjustY, Rotation2d())).X(),
+                            poseDirection.transformBy(Transform2d(adjustX, -adjustY, Rotation2d())).Y(),
+                            level[0]
+                        ),
+                        Rotation3d(
+                            0,
+                            degreesToRadians(level[1]),
+                            poseDirection.rotation().radians()
+                        )
+                    )
+                )
+                branchPositions.append((face * 2) + 1, fillRight)
+                branchPositions.append((face * 2) + 2, fillLeft)
+
+    class StagingPositions:
+        leftIceCream  = Pose2d(Translation2d(inchesToMeters(48), inchesToMeters(230.5), Rotation2d()))
+        middleIceCream = Pose2d(Translation2d(inchesToMeters(48), inchesToMeters(158.5), Rotation2d()))
+        rightIceCream = Pose2d(Translation2d(inchesToMeters(48), inchesToMeters(86.5), Rotation2d()))
+
+    class ReefHeight: # this is not used
+        class L4:
+            height = inchesToMeters(72)
+            pitch = -90
+        class L3:
+            height = inchesToMeters(47.625)
+            pitch = -35
+        class L2:
+            height = inchesToMeters(31.875)
+            pitch = -35
+        class L1:
+            height = inchesToMeters(18)
+            pitch = 0
