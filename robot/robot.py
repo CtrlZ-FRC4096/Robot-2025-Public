abbb4d4f94289fbf2d1e89d5f29cbfd0c122fb15
EugeneZed <ctrlz4096@gmail.com>
2025-02-01 16:59:46 -0600

    changed auto name

--- robot/robot.py
+++ robot/robot.py
@@ -159,7 +159,7 @@ class Robot(CoroutineRobot):
         )
 
         ## Need to change this and redeloy
-        self.path = PathPlannerAuto("Test Auto")  # centerline steal 2nd match
+        self.path = PathPlannerAuto("4 piece auto test")  # centerline steal 2nd match
 
         self.in_autonomous_mode = False
 
