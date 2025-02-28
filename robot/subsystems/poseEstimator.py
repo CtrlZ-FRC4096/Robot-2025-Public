74e8f4a6cc0b5536b1adece1c2c8b434c574d50c
eziCode <ezraakresh@gmail.com>
2025-02-28 13:13:44 -0600

    changed to <2

--- robot/subsystems/poseEstimator.py
+++ robot/subsystems/poseEstimator.py
@@ -441,7 +441,7 @@ class PoseEstimator(Subsystem):
                 return target_pose
 
     def useSingleTag(self):
-        return (self.curEstPoseGlobal - self.tag_layout.getTagPose(self.calculate_closest_reef_tag()[0]).toPose2d()).translation().norm() > 2
+        return (self.curEstPoseGlobal - self.tag_layout.getTagPose(self.calculate_closest_reef_tag()[0]).toPose2d()).translation().norm() < 2
 
     def periodic(self):
         allianceColor = DriverStation.getAlliance()
