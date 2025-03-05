bc507721c789f5bdac7975d2dbedb61521317766
John Doe <johndoe@email.com>
2025-03-04 22:04:04 -0600

    scoring adjustments

--- robot/robot_scoring_positions.py
+++ robot/robot_scoring_positions.py
@@ -1,10 +1,10 @@
 class RobotScoringPositions:
 	elevator_raise_threshold = 1.0 # meters needed to be closer than to reef to raise elevator
-	end_effector_travel_position = 8.0 # 8.0
+	end_effector_travel_position = 8.1 # 8.0
 	elevator_intake_height = 3.0
 	end_effector_intake_position = 0.0
 	min_elevator_height_to_bring_in_end_effector = 4.0
-	min_end_effector_position_to_move_elevator_up = 8.2
+	min_end_effector_position_to_move_elevator_up = 7.5
 	class L1_Scoring:
 		elevator_height = 7.18
 		end_effector_outtake_speed = 20
