0f561a80ca29f19a2aa10bf98c3dad0df33d045a
eziCode <ezraakresh@gmail.com>
2025-03-07 12:14:41 -0600

    wrote climber subsystem draft

--- robot/subsystems/climber.py
+++ robot/subsystems/climber.py
@@ -25,44 +25,49 @@ class Climber(Subsystem):
     def __init__(self, robot: "Robot"):
         super().__init__()
         self.robot = robot
-        self.climber_winch_motor = hardware.TalonFX(const.CLIMBER_WINCH_MOTOR_CAN_ID, "rio")
+        self.climber_arm_motor = hardware.TalonFX(const.CLIMBER_ARM_MOTOR_CAN_ID, "rio")
         self.climber_intake_motor = hardware.TalonFX(const.CLIMBER_INTAKE_MOTOR_CAN_ID, "rio")
 
-        self.climber_winch_motor_config = configs.TalonFXConfiguration()
+        self.climber_arm_motor_config = configs.TalonFXConfiguration()
 
         ## First we will set everything to zero and then adjust k_g until the elevator is able to hold its position when we command a position
-        self.climber_winch_motor_config.slot0.k_g = 0.45
+        self.climber_arm_motor_config.slot0.k_g = 0.45
 
         ## Next we will adjust k_s until the elevator just barely moves when we command a position (check both up and down)
-        self.climber_winch_motor_config.slot0.k_s = 0.0
+        self.climber_arm_motor_config.slot0.k_s = 0.0
 
         ## Next we will adjust k_p until the elevator moves to the correct position and slightly overshoots/oscillates
-        self.climber_winch_motor_config.slot0.k_p = 7.0
+        self.climber_arm_motor_config.slot0.k_p = 7.0
         ## Next we will adjust k_d until the elevator moves to the correct position without overshooting/oscillating
-        self.climber_winch_motor_config.slot0.k_d = 0.0
+        self.climber_arm_motor_config.slot0.k_d = 0.0
 
         ## Adjust other stuff if we need it like k_v and k_a for feed forward
-        self.climber_winch_motor_config.current_limits.supply_current_limit = 80
-        self.climber_winch_motor_config.torque_current.peak_forward_torque_current = 80
-        self.climber_winch_motor_config.torque_current.peak_reverse_torque_current = -80
+        self.climber_arm_motor_config.current_limits.supply_current_limit = 80
+        self.climber_arm_motor_config.torque_current.peak_forward_torque_current = 80
+        self.climber_arm_motor_config.torque_current.peak_reverse_torque_current = -80
         ##Ramps
-        self.climber_winch_motor_config.closed_loop_ramps.torque_closed_loop_ramp_period = 0.02
-        self.climber_winch_motor_config.open_loop_ramps.torque_open_loop_ramp_period = 0.02
-        self.climber_winch_motor_config.closed_loop_ramps.duty_cycle_closed_loop_ramp_period = 0.02
-        self.climber_winch_motor_config.open_loop_ramps.duty_cycle_open_loop_ramp_period = 0.02
-        self.climber_winch_motor_config.closed_loop_ramps.voltage_closed_loop_ramp_period = 0.02
-        self.climber_winch_motor_config.open_loop_ramps.voltage_open_loop_ramp_period = 0.02
-        self.climber_winch_motor_config.current_limits.supply_current_limit_enable = True
-        self.climber_winch_motor_config.motor_output.neutral_mode = signals.NeutralModeValue(1)
-        self.climber_winch_motor_config.current_limits.stator_current_limit = 100
+        self.climber_arm_motor_config.closed_loop_ramps.torque_closed_loop_ramp_period = 0.02
+        self.climber_arm_motor_config.open_loop_ramps.torque_open_loop_ramp_period = 0.02
+        self.climber_arm_motor_config.closed_loop_ramps.duty_cycle_closed_loop_ramp_period = 0.02
+        self.climber_arm_motor_config.open_loop_ramps.duty_cycle_open_loop_ramp_period = 0.02
+        self.climber_arm_motor_config.closed_loop_ramps.voltage_closed_loop_ramp_period = 0.02
+        self.climber_arm_motor_config.open_loop_ramps.voltage_open_loop_ramp_period = 0.02
+        self.climber_arm_motor_config.current_limits.supply_current_limit_enable = True
+        self.climber_arm_motor_config.motor_output.neutral_mode = signals.NeutralModeValue(1)
+        self.climber_arm_motor_config.current_limits.stator_current_limit = 100
 
-        self.climber_winch_motor_config.motor_output.inverted = signals.InvertedValue(1)
+        self.climber_arm_motor_config.motor_output.inverted = signals.InvertedValue(1)
 
         # We will adjust these values later to get the elevator moving faster
-        self.climber_winch_motor_config.motion_magic.motion_magic_cruise_velocity = 175 # Recalc has us at 16 RPS, but starting slow
-        self.climber_winch_motor_config.motion_magic.motion_magic_acceleration = 100 # Recalc has us at 100 RPS/s^2, but starting slow
+        self.climber_arm_motor_config.motion_magic.motion_magic_cruise_velocity = 175 # Recalc has us at 16 RPS, but starting slow
+        self.climber_arm_motor_config.motion_magic.motion_magic_acceleration = 100 # Recalc has us at 100 RPS/s^2, but starting slow
 
-        self.climber_winch_motor.configurator.apply(self.climber_winch_motor_config)
+        self.climber_arm_motor.configurator.apply(self.climber_arm_motor_config)
+
+        self.gear_ratio = 140.0 # need to adjust if using something else
+        self.command_position = 0.0
+        self.request = controls.MotionMagicVoltage(0, enable_foc=True)
+
 
         self.climber_intake_motor_config = configs.TalonFXConfiguration()
         self.climber_intake_motor_config.motor_output.inverted = signals.InvertedValue(1)
@@ -83,6 +88,16 @@ class Climber(Subsystem):
         self.climber_intake_motor_config.motor_output.inverted = signals.InvertedValue(1)
 
         self.climber_intake_motor.configurator.apply(self.climber_intake_motor_config)
+    
+    def set_climber_position(self, position):
+        self.command_position = position
+        rotation = self.command_position * self.gear_ratio
+        self.climber_arm_motor.set_control(self.request.with_position(rotation))
+
+    def get_position(self):
+        rotations = self.climber_arm_motor.get_position().value
+        height = rotations / self.gear_ratio
+        return height
 
     def stop(self):
         pass
