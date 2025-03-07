43bc72e3972e997119d7ec279784ec3b6c2d02c7
eziCode <ezraakresh@gmail.com>
2025-03-07 12:44:19 -0600

    tush push auto (may need to tweak first path)

--- robot/autoroutines.py
+++ robot/autoroutines.py
@@ -66,3 +66,28 @@ class AutoRoutines:
             self.robot.coroutines.score_piece_3.withTimeout(5.0),
             self.robot.coroutines.reset_robot_after_scoring_3,
         )
+    
+    def tush_push_auto(self):
+        return SequentialCommandGroup(
+            self.robot.followPathCommand("tp_1"),
+            self.robot.coroutines.score_left_branch_1,
+            self.robot.coroutines.score_L4_1,
+            self.robot.coroutines.score_piece_1.withTimeout(5.0),
+            self.robot.coroutines.reset_robot_after_scoring_1,
+            self.robot.followPathCommand("3P_2"),
+            self.robot.coroutines.intake_coral_1,
+            self.robot.coroutines.reset_robot_after_intaking_1,
+            self.robot.followPathCommand("3P_3"),
+            self.robot.coroutines.score_right_branch,
+            self.robot.coroutines.score_L4_2,
+            self.robot.coroutines.score_piece_2.withTimeout(5.0),
+            self.robot.coroutines.reset_robot_after_scoring_2,
+            self.robot.followPathCommand("3P_4"),
+            self.robot.coroutines.intake_coral_2,
+            self.robot.coroutines.reset_robot_after_intaking_2,
+            self.robot.followPathCommand("3P_5"),
+            self.robot.coroutines.score_left_branch_2,
+            self.robot.coroutines.score_L4_3,
+            self.robot.coroutines.score_piece_3.withTimeout(5.0),
+            self.robot.coroutines.reset_robot_after_scoring_3,
+        )
