8b8418c2d9ff9f60d281204bc2595b405896b8bc
eziCode <ezraakresh@gmail.com>
2025-03-08 19:22:41 -0600

    changed ee logic

--- robot/subsystems/end_effector.py
+++ robot/subsystems/end_effector.py
@@ -182,6 +182,8 @@ class EndEffector(Subsystem):
                     self.stop()
                     self.set_end_effector_position(RobotScoringPositions.end_effector_travel_position)
                     self.robot.has_coral = True
+            else:
+                self.robot.elevator.set_elevator_height(RobotScoringPositions.elevator_intake_height)
         elif self.robot.is_climbing:
             if self.robot.elevator.get_height() <= RobotScoringPositions.min_elevator_height_to_bring_in_end_effector:
                 self.set_end_effector_position(RobotScoringPositions.end_effector_climbing_position)
